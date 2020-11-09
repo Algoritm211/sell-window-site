5066690284370144df5c5547f29cef6062fd36b2
Algoritm211 <60394886+Algoritm211@users.noreply.github.com>
2020-11-09 22:57:02 +0200

    Add checking data

--- src/js/main.js
+++ src/js/main.js
@@ -8,15 +8,18 @@ import './slider'
 
 document.addEventListener('DOMContentLoaded', () => {
 
-  const modalState = {}
+  const modalState = {
+    form: 0,
+    windowType: 'tree',
+  }
 
   changeModalState(modalState)
-  modals()
+  modals(modalState)
   tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active')
   tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click')
   tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block')
   forms(modalState)
   timer('#timer', '2020-12-31')
-  
+
   images()
 })
--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,6 +1,6 @@
-const modals = () => {
+const modals = (state) => {
 
-  function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true, display = 'block') {
+  function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay=true) {
 
     const trigger = document.querySelectorAll(triggerSelector)
     const modal = document.querySelector(modalSelector)
@@ -17,7 +17,7 @@ const modals = () => {
           item.style.display = 'none'
         })
   
-        modal.style.display = display
+        modal.style.display = 'block'
         document.body.style.overflow = 'hidden'
         // document.body.classList.add('modal-open')
       })
@@ -44,6 +44,31 @@ const modals = () => {
     })
   }
 
+  function checkStateData(buttonWrapperSelector, property) {
+    const buttonWrapper = document.querySelectorAll(buttonWrapperSelector)
+    const message = {
+      height: 'Не указана высота',
+      width: 'Не указана длина',
+      windowProfile: 'Не указан тип остекления',
+    }
+    let statusMessage = document.createElement('div')
+    statusMessage.classList.add('status')
+    console.log('sdf');
+
+    buttonWrapper.forEach(item => {
+      item.addEventListener('mouseenter', (event) => {
+          if (!state.hasOwnProperty(property)) {
+            item.appendChild(statusMessage)
+            statusMessage.textContent = message[property]
+            item.children[0].setAttribute('disabled', true)
+          } else {
+            item.children[0].removeAttribute('disabled')
+            statusMessage.remove()
+          }
+      })
+    })
+  }
+
   function showModalByTime(selector, time) {
     setTimeout(() => {
       document.querySelector(selector).style.display = display
@@ -54,9 +79,13 @@ const modals = () => {
   bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close')
   bindModal('.phone_link', '.popup', '.popup .popup_close')
   bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close')
-  bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false)
+  bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false, true, '.popup_calc_content')
   bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false)
   // showModalByTime('.popup', 60000)
+  checkStateData('.check-data-form', 'height')
+  checkStateData('.check-data-form', 'width')
+  checkStateData('.check-window-profile-form', 'windowProfile')
+
 }
 
 export default modals
